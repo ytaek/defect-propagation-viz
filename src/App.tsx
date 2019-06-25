748ab36b100ecbabf86f3d4b86b6c4068e26bdfa
ytaek.kim <ytaek.kim@hcil.snu.ac.kr>
2019-06-25 23:23:08 +0900

    add test project data set

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import './App.css';
-import { DataService, ProjectInterface } from './services/DataService';
+import { DataService } from './services/DataService';
 import CriterionListContainer from './containers/CriterionListContainer';
 import ProjectListContainer from './containers/ProjectListContainer';
 
@@ -15,18 +15,18 @@ class App extends React.Component {
   render() {
 console.log("app", this.dataService.criteriaData);
 
-    const t: ProjectInterface = {
-      code:"1234", score:0, isPropCandidate:false, 
-      attributes: undefined 
-    };
-console.log("t", t);
+    // const t: ProjectInterface = {
+    //   code:"1234", score:0, isPropCandidate:false, 
+    //   attributes: undefined 
+    // };
+// console.log("t", t);
     return (
       <div className="App-container">
         <div className="App-sidebar">
           <CriterionListContainer criteriaData={this.dataService.criteriaData}/>
         </div>
         <div className="App-main">
-          <ProjectListContainer projectsData={[t]} />
+          <ProjectListContainer projectsData={this.dataService.projectsData} />
         </div>
       </div>
     );
--- src/services/DataService.ts
+++ src/services/DataService.ts
@@ -5,7 +5,7 @@ export interface MetaDatumInterface {
 
 export interface CriterionValueInterface {
   name: string;
-  weight: number;    
+  weight: number;
 }
 
 export interface CriterionInterface {
@@ -21,42 +21,76 @@ export interface ProjectInterface {
   attributes: any;
 }
 
-export interface ProjectAttributesInterface {
+export class ProjectAttributes {
   product: string;
-  // productApply: string;
-  // imsProjectName: string;
+  productApply: string;
+  imsProjectName: string;
   // parentProjectCode: string;
   oem: string;
-  // FWName: string;
+  fwVersion: string;
   capacity: string;
   density: string;
   formFactor: string;
   nandCell: string;
   nandDesign: string;
-}
 
+  constructor() {
+    this.product = "";
+    this.productApply = "";
+    this.imsProjectName = "";
+    this.oem = "";
+    this.fwVersion = "";
+    this.capacity = "";
+    this.density = "";
+    this.formFactor = "";
+    this.nandCell = "";
+    this.nandDesign = "";
+  }
+}
 
 export class DataService {
   private _criteriaData: CriterionInterface[];
+  private _projectsData: ProjectInterface[];
 
   constructor() {
-    const metaData = require('../data/meta.json');
+    const metaData = require("../data/meta.json");
+    const prjData = require("../data/projects.json");
 
-    this._criteriaData = metaData.map( (d: MetaDatumInterface) => {
-      const cValues: CriterionValueInterface[] = 
-        d.values.map( (item: string) => ({ name: item, weight: 0 }) );
+    this._criteriaData = metaData.map((d: MetaDatumInterface) => {
+      const cValues: CriterionValueInterface[] = d.values.map(
+        (item: string) => ({ name: item, weight: 0 })
+      );
 
       const criterion: CriterionInterface = {
         name: d.name,
         values: cValues,
         weight: 0
-      }
+      };
       return criterion;
     });
+console.log("keys", Object.keys(new ProjectAttributes()));
+    this._projectsData = prjData.map( (d: any) => {
+      const prjAttr = Object.keys(new ProjectAttributes()).reduce( (prev, cur) => {
+            prev[cur] = d[cur];
+            return prev;
+          }, {});
+      return {
+        code: d.code,
+        score: 0,
+        isPropCandidate: false,
+        attributes: prjAttr
+      };
+    });
+
+    console.log("prjData", prjData, this._projectsData);
   }
 
   get criteriaData(): CriterionInterface[] {
     return this._criteriaData;
+  }
+
+  get projectsData(): ProjectInterface[] {
+    return this._projectsData;
   }
 }
 
