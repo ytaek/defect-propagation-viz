81482f06b9a8d480ddb7a09e66cd73c1f56484be
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2019-07-12 16:41:31 +0900

    implementing weight slider

--- src/components/CriterionValueWeightControl.tsx
+++ src/components/CriterionValueWeightControl.tsx
@@ -19,24 +19,30 @@ export class CriterionValueWeightControl extends React.Component<Props> {
   }
 
   render() {
+    let weightView
+    if(this.props.criterionValue.status === CriterionValueStatus.WEIGHT){
+      weightView = <div className="weight-slider"/>
+    }
+
     return (
-      <div className="category btn btn-light">
+      <div className="category">
         <div className="name">{this.props.criterionValue.name}</div>
         <div className="status-container">
           <div className="status-button" onClick={this.handleClick}>
             {this.renderStatusSwitch(this.props.criterionValue.status)}
           </div>
-          <div className="weight">{this.props.criterionValue.weight}</div>
+          {weightView}
         </div>
       </div>)
   }
 
   renderStatusSwitch(status: CriterionValueStatus){
+    console.log("status: " , status)
     switch(status){
       case CriterionValueStatus.CAND:
         return (<div className="status status-cand"/>)
       case CriterionValueStatus.NONCAND:
-        return (<div className="status status-noncand">W</div>)
+        return (<div className="status status-noncand">×</div>)
       case CriterionValueStatus.WEIGHT:
         default:
           return (<div className="status status-weight"/>)
