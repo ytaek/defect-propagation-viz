1b802c40d6569fdf224b0d0a50b7169b9d66574b
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2019-07-25 15:52:57 +0900

    added threshold slider

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -6,33 +6,91 @@ import * as d3 from "d3";
 import { ProjectsState } from 'src/redux/modules/projects';
 import { Col, Row } from "react-bootstrap";
 
+const CANDIDATE_WEIGHT_WIDTH = 150
+
 interface Props {
   projectState: ProjectsState;
   criteria: CriterionInterface[];
   onOrder(): void;
 }
 
-class ProjectList extends React.Component<Props> {
+interface State {
+  immediateThresholdScore: number | null
+}
+
+class ProjectList extends React.Component<Props, State> {
+
+  private mouseDownSliderX: number
+  private mouseDownClientX: number
+  private sliderSize: number
+
+  constructor(props: Props) {
+    super(props)
+
+    this.state = { immediateThresholdScore: null }
+
+    this.mouseDownOnThresholdBar = this.mouseDownOnThresholdBar.bind(this)
+
+    this.mouseMoveForThresholdBar = this.mouseMoveForThresholdBar.bind(this)
+
+    this.mouseUpForThresholdBar = this.mouseUpForThresholdBar.bind(this)
+  }
+
+  mouseDownOnThresholdBar(event: any) {
+    this.mouseDownSliderX = event.nativeEvent.offsetX
+    this.mouseDownClientX = event.nativeEvent.clientX
+    this.sliderSize = event.nativeEvent.srcElement.offsetWidth
+
+    window.addEventListener('mouseup', this.mouseUpForThresholdBar, false);
+    window.addEventListener('mousemove', this.mouseMoveForThresholdBar, false);
+  }
+
+  mouseMoveForThresholdBar(event: any) {
+    const deltaX = event.clientX - this.mouseDownClientX
+
+    this.setState({
+      immediateThresholdScore: Math.min(1, Math.max(0, (this.mouseDownSliderX + deltaX) / this.sliderSize)) * 100
+    })
+  }
+
+  mouseUpForThresholdBar(event: any) {
+    this.mouseMoveForThresholdBar(event)
+
+    this.changeThresholdScore(this.state.immediateThresholdScore!)
+
+    window.removeEventListener("mouseup", this.mouseUpForThresholdBar, false);
+    window.removeEventListener("mousemove", this.mouseMoveForThresholdBar, false);
+  }
+
+  changeThresholdScore(value: number) {
+    // TODO connect
+    return;
+  }
 
   render() {
     const { projectState: projectState, criteria } = this.props;
     const projects = projectState.projects;
-    const thresholdScore = projectState.thresholdScore; 
+
+    // TODO uncomment this row
+    // const thresholdScore = projectState.thresholdScore || this.state.immediateThresholdScore; 
+    const thresholdScore = this.state.immediateThresholdScore || 0;
+
+
     const colors = d3.schemePastel2;
     const darkColors = d3.schemeSet2;
 
     const candidateCriterionValueList = this.getCriterionValueListByStatus(CriterionValueStatus.CAND);
     const nonCandidateCriterionValueList = this.getCriterionValueListByStatus(CriterionValueStatus.NONCAND);
     const weightCriterionValueList = this.getCriterionValueListByStatus(CriterionValueStatus.WEIGHT);
-    
+
     // sort projects
-    projects.forEach( project => {
+    projects.forEach(project => {
       let candCnt = 0;
       let nonCandCnt = 0;
-      const candidates = this.props.criteria.map( (c) => {
-        const cvList = candidateCriterionValueList.filter( cv => cv.criterion!.name === c.name).map( d => d.name);
-        const ncvList = nonCandidateCriterionValueList.filter( cv => cv.criterion!.name === c.name).map( d => d.name);
-  
+      const candidates = this.props.criteria.map((c) => {
+        const cvList = candidateCriterionValueList.filter(cv => cv.criterion!.name === c.name).map(d => d.name);
+        const ncvList = nonCandidateCriterionValueList.filter(cv => cv.criterion!.name === c.name).map(d => d.name);
+
         if (cvList.includes(project.attributes[c.name])) {
           candCnt++;
           return "O";
@@ -41,10 +99,10 @@ class ProjectList extends React.Component<Props> {
           nonCandCnt++;
           return "X";
         }
-        else {return "-";}
+        else { return "-"; }
       });
       project.candidateOXList = candidates;
-      
+
       if (candCnt > 0 && nonCandCnt === 0) {
         project.candidateStatus = ProjectCandidateStatus.RULE;
         project.isCandidate = true;
@@ -61,26 +119,26 @@ class ProjectList extends React.Component<Props> {
         project.candidateRuleScore = candCnt - nonCandCnt;
       }
     });
-    
+
     const candProjectsByRule = projects.filter(p => (
       p.candidateStatus === ProjectCandidateStatus.RULE
       && p.isCandidate))
       .sort((a, b) => (b.score - a.score))
       .sort((a, b) => (b.candidateRuleScore! - a.candidateRuleScore!))
       .map(d => d);
-    
+
     const greyProjects = projects.filter(p => (
       p.candidateStatus !== ProjectCandidateStatus.RULE))
       .sort((a, b) => (b.score - a.score))
       .map(d => d);
-    
+
     const nonCandProjectsByRule = projects.filter(p => (
       p.candidateStatus === ProjectCandidateStatus.RULE
       && !p.isCandidate))
       .sort((a, b) => (b.score - a.score))
       .sort((a, b) => (b.candidateRuleScore! - a.candidateRuleScore!))
       .map(d => d);
-    
+
     console.log(candProjectsByRule, greyProjects, nonCandProjectsByRule);
 
     return (
@@ -109,25 +167,25 @@ class ProjectList extends React.Component<Props> {
           <div className="project-name"><b>NAME</b></div>
           <div className="candidate-ox">
             <b>CANDIDATE</b> <br />
-            {this.props.criteria.map( (d: any, i:number) => (
-              <b key={i} style={{color:darkColors[i]}}>{i+1} </b>
+            {this.props.criteria.map((d: any, i: number) => (
+              <b key={i} style={{ color: darkColors[i] }}>{i + 1} </b>
             ))}
           </div>
           <div className="score">
-            <b>SCORE BAR<br/>(Min:-100, Max:100)</b>
+            <b>SCORE BAR<br />(Min:-100, Max:100)</b>
           </div>
           <div>
-            <div className="bars" style={{justifyContent: "flex-left"}}>
+            <div className="bars" style={{ justifyContent: "flex-left" }}>
               {
-                criteria.map( (c, i) => (
+                criteria.map((c, i) => (
                   <div key={i} className="attributes">
                     <div style={{
-                        margin: "2px",
-                        fontWeight:"bold",
-                        textAlign:"center",
-                        width:60*c.weight,
-                        backgroundColor: colors[i]
-                      }}>
+                      margin: "2px",
+                      fontWeight: "bold",
+                      textAlign: "center",
+                      width: 60 * c.weight,
+                      backgroundColor: colors[i]
+                    }}>
                       {c.weight !== 0 ? c.name : ""}
                     </div>
                   </div>
@@ -136,48 +194,68 @@ class ProjectList extends React.Component<Props> {
             </div>
             <div className="bars">
               <div className="header-bars" />
-              <div className="cand-separator" style={{height:3}}/>
+              <div className="cand-separator" style={{ height: '30px' }} />
               <div className="header-bars" />
             </div>
           </div>
         </div>
         <div className="cand-zone">
-        {
-          // sorting 
-          // cand - grey - noncand
-          candProjectsByRule.map( (prj: ProjectInterface, i: number) => 
-            <ProjectItem 
-              project={prj}
-              criteria={criteria}
-              key={i}/>
-          )
-        }
+          {
+            // sorting 
+            // cand - grey - noncand
+            candProjectsByRule.map((prj: ProjectInterface, i: number) =>
+              <ProjectItem
+                project={prj}
+                criteria={criteria}
+                key={i} />
+            )
+          }
         </div>
-        <div className="separator"/>
+        <div className="separator">
+          <div className="slider-title">Set Threshold Score</div>
+          <div className="slider-container">
+            <div className="drag-region"  onMouseDown={this.mouseDownOnThresholdBar}/>
+            <div className="candidate-region-indicator"
+              style={{
+                width: (100 - thresholdScore) + "%",
+                left: thresholdScore + "%"
+              }}
+            />
+            <div className="indicator-text current-score" style={{
+              left: thresholdScore + "%"
+            }}>{Math.round(thresholdScore * 100)/100}</div>
+            <div className="indicator-text min-score">0</div>
+            <div className="indicator-text max-score">100</div>
+            <div className="indicator-icon" style={{
+              left: thresholdScore + "%"
+            }}/>
+
+          </div>
+        </div>
         <div className="grey-zone">
-        {
-          // sorting 
-          // cand - grey - noncand
-          greyProjects.map( (prj: ProjectInterface, i: number) => 
-            <ProjectItem 
-              project={prj}
-              criteria={criteria}
-              key={i}/>
-          )
-        }
+          {
+            // sorting 
+            // cand - grey - noncand
+            greyProjects.map((prj: ProjectInterface, i: number) =>
+              <ProjectItem
+                project={prj}
+                criteria={criteria}
+                key={i} />
+            )
+          }
         </div>
         <div className="separator" />
         <div className="noncand-zone">
-        {
-          // sorting 
-          // cand - grey - noncand
-          nonCandProjectsByRule.map( (prj: ProjectInterface, i: number) => 
-            <ProjectItem 
-              project={prj}
-              criteria={criteria}
-              key={i}/>
-          )
-        }
+          {
+            // sorting 
+            // cand - grey - noncand
+            nonCandProjectsByRule.map((prj: ProjectInterface, i: number) =>
+              <ProjectItem
+                project={prj}
+                criteria={criteria}
+                key={i} />
+            )
+          }
         </div>
       </div>
     );
@@ -186,8 +264,8 @@ class ProjectList extends React.Component<Props> {
   private getCriterionValueListByStatus(status: CriterionValueStatus): CriterionValueInterface[] {
     const cvList: CriterionValueInterface[] = [];
     return this.props.criteria.reduce(
-        (prev, cv) => (prev.concat(cv.values)), cvList)
-        .filter(cv => (cv.status === status));
+      (prev, cv) => (prev.concat(cv.values)), cvList)
+      .filter(cv => (cv.status === status));
   }
 }
 
